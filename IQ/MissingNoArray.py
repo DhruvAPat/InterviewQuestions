20efc4c0bdf6dc0b6c7b6b016df19d5ce2b87ca2
hello <patil.dhruv49@gmail.com>
2022-07-24 15:42:14 +0530

    Commit

---
+++ IQ/MissingNoArray.py
@@ -0,0 +1,13 @@
+#Using Sum of n natural numbers
+def missarray(arr):
+    l=len(arr)
+    natural=[]
+    for i in range(1,l+2):
+        natural.append(i)
+    
+    s=sum(natural)
+    s1=sum(arr)
+    target=s-s1
+    return target
+arr=[1,2,3,4,5,6,7,8,10]
+print("The missing no in the array is",missarray(arr))
